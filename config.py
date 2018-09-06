a330e95ec7f192dd4c8cb5e29c495b19860c99ab
paynes <jan.koscak@gmail.com>
2018-09-06 22:21:22 +0200

    created pagination and post form

--- config.py
+++ config.py
@@ -10,4 +10,6 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+	POSTS_PER_PAGE = 3
+
 
